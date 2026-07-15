c4b14202b3e9a48b5349400676bca2297456067c
mathiasbraun <mvjbraun@gmail.com>
2026-07-15 15:24:57 +0200

    Add: python-loop-unpacking - submission-2

---
+++ Python Coding Interviews/python-loop-unpacking/submission-2.py
@@ -0,0 +1,17 @@
+from typing import List, Tuple
+
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    best, name = 0, None
+    for student_score in scores:
+        student, score = student_score
+        if score > best:
+            best, name = score, student
+    return name
+
+
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
