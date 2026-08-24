6bdb10a56193294738ce6b75d84169bbb42044b2
mathiasbraun <mvjbraun@gmail.com>
2026-08-24 17:56:33 +0200

    Add: move-zeroes - submission-6

---
+++ Data Structures & Algorithms/move-zeroes/submission-6.py
@@ -0,0 +1,6 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        for num in nums:
+            if num == 0:
+                nums.remove(num)
+                nums.append(0)
