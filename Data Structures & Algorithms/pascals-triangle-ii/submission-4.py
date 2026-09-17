44c8cf8ee99176364841b00b23dabf43404e952d
mathiasbraun <mvjbraun@gmail.com>
2026-09-17 14:12:39 +0200

    Add: pascals-triangle-ii - submission-4

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-4.py
@@ -0,0 +1,8 @@
+import math
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        res = []
+        for i in range(rowIndex + 1):
+            res.append(math.comb(rowIndex, i))
+        return res
